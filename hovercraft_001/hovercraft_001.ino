0ad51036ceaf2dffaaa644bd24d19bc936d036fe
Shreyas Balaji <shreyasb99@gmail.com>
2014-06-14 01:41:19 -0500

    changes

--- hovercraft_001/hovercraft_001.ino
+++ hovercraft_001/hovercraft_001.ino
@@ -3,3 +3,17 @@
 // Commentation
 // Project HoverCraft
 
+
+
+#define RIGHT    0
+#define FORWARD  1
+#define LEFT     2
+#define BACKWARD 3
+#define STANDARD 1
+#define REVERSE  2
+
+int [][] motors = int [4][2];
+
+void move(int[][] params)  {
+  
+}
